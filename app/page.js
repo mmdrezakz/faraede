a59790e18e6dc7c0cc0d6dead53be521652521e0
Mohammadreza <mellboybeatprod@gmail.com>
2025-12-26 03:21:02 +0330

    Add responsive style

--- app/page.js
+++ app/page.js
@@ -10,10 +10,10 @@ export default function LandingPage() {
       <Navbar />
 
 {/* Hero Section */}
-<section className="relative grid  grid-cols-1 md:grid-cols-3   md:mt-50 section1 text-center section2   h-auto md:h-[65vh] px-6 md:py-10">
+<section className="relative grid  grid-cols-1 md:grid-cols-3   md:mt-50 section1 text-center section2   h-auto  px-6 py-10 ">
 
   {/* Text */}
-  <div className="  my-30">
+  <div className="  2xl:my-30">
     <h1 className=" text-6xl lg:text-8xl font-bold mt-10 text-gray-100 ">فرا تر از انتظار</h1>
     <h2 className="text-4xl md:text-6xl  text-gray-200 ">فرا ایده</h2>
     <button className="bg-gray-400 mt-1 text-gray-900 px-6 py-3 rounded-full hover:bg-gray-300 transition duration-500">
